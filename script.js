db04abb4175720c537f77fc170f7357b3b8ce9d9
root <root@ignea.huawei.net>
2019-02-08 14:00:49 -0600

    Commit inicial

---
+++ script.js
@@ -0,0 +1,113 @@
+/*
+ * Fuente: http://jsfiddle.net/jonataswalker/w5uuxhov/
+ *
+*/
+var mapLat = 19.5712388;
+var mapLng = -99.2530477;
+var mapDefaultZoom = 10;
+
+var styles = {
+    'Icon': [new ol.style.Style({
+        image: new ol.style.Icon({
+	anchor: [0.5, 0.5],
+        anchorXUnits: "fraction",
+        anchorYUnits: "fraction",
+        src: './logoheaderzorro_40x21px.png', 
+        //src: feature.getGeometry('Icon'),
+      }),
+    })],
+    'Point': [new ol.style.Style({
+        image: new ol.style.Circle({
+            radius: 8,
+            fill: new ol.style.Fill({
+                //color: [255, 255, 255, 0.3]
+                color: 'red', 
+            }),
+            stroke: new ol.style.Stroke({color: 'pink', width: 2})
+        })
+    })],
+};
+
+var styleFunction = function(feature, resolution) {
+  return styles[feature.getGeometry().getType()];
+};
+
+var olview = new ol.View({
+    center: ol.proj.fromLonLat([mapLng, mapLat]),
+    //center: [-99.25, 19.57],
+    zoom: 13,
+    minZoom: 2,
+    maxZoom: 20
+});
+
+var geojson_layer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: 'dataz.geojson',
+        //url: 'file.geojson',
+        format: new ol.format.GeoJSON()
+    }),
+    style: styleFunction
+});
+
+var map = new ol.Map({
+    target: document.getElementById('map'),
+    view: olview,
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      }), geojson_layer
+    ]
+});
+
+/**
+ * Popup
+ **/
+var
+    container = document.getElementById('popup'),
+    content_element = document.getElementById('popup-content'),
+    closer = document.getElementById('popup-closer');
+
+closer.onclick = function() {
+    overlay.setPosition(undefined);
+    closer.blur();
+    return false;
+};
+var overlay = new ol.Overlay({
+    element: container,
+    autoPan: true,
+    offset: [0, -10]
+});
+map.addOverlay(overlay);
+
+var fullscreen = new ol.control.FullScreen();
+map.addControl(fullscreen);
+
+map.on('click', function(evt){
+    var feature = map.forEachFeatureAtPixel(evt.pixel,
+      function(feature, layer) {
+        return feature;
+      });
+    if (feature) {
+        var geometry = feature.getGeometry();
+        var coord = geometry.getCoordinates();
+        
+        var content = '<h4>' + feature.get('Cadena') + '</h4>';
+        content += '<h5>Tienda: ' + feature.get('Tienda') + '</h5>';
+        content += '<p>' + feature.get('Municipio');
+        content += ', ' + feature.get('Estado') + '</p>';
+        
+        content_element.innerHTML = content;
+        overlay.setPosition(coord);
+        
+        console.info(feature.getProperties());
+    }
+});
+map.on('pointermove', function(e) {
+    if (e.dragging) return;
+       
+    var pixel = map.getEventPixel(e.originalEvent);
+    var hit = map.hasFeatureAtPixel(pixel);
+    
+    map.getTarget().style.cursor = hit ? 'pointer' : '';
+});
+
